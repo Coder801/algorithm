330cd81298712814c5e299f14ea0800555103490
Dmitriy Onischenko <donischenko801@gmail.com>
2017-12-12 16:23:48 +0200

    Code review

--- src/searching/interpolation/interpolation.js
+++ src/searching/interpolation/interpolation.js
@@ -1,24 +1,25 @@
 module.exports = function interpolationSearch(array, needle) {
-    var middle;
-    var low = 0;
-    var high = array.length - 1;
+  var middle;
+  var low = 0;
+  var high = array.length - 1;
 
-    while (array[low] < needle && array[high] > needle)
-    {  middle = low + Math.floor( ((needle - array[low]) * (high - low))/(array[high]- array[low]) );
-       if (array[middle] < needle) {
-         low = middle + 1;
-       } else if (array[middle] > needle) {
-         high = middle - 1;
-       } else {
-         return middle;
-       }
-    }
+  while (array[low] < needle && array[high] > needle) {
 
-    if (array[low] === needle) {
-      return low;
-    } else if (array[high] === needle) {
-      return high;
+    middle = low + Math.floor(((needle - array[low]) * (high - low)) / (array[high] - array[low]));
+    if (array[middle] < needle) {
+      low = middle + 1;
+    } else if (array[middle] > needle) {
+      high = middle - 1;
     } else {
-      return -1;
+      return middle;
     }
+  }
+
+  if (array[low] === needle) {
+    return low;
+  } else if (array[high] === needle) {
+    return high;
+  } else {
+    return -1;
+  }
 }
